82fdee12711441f90976571b7b26dd5fbd07b0c4
TheRealFalseReality <NicholasWhite@capitalcityaquatics.com>
2022-06-23 21:31:33 -0400

    1

---
+++ app/src/main/java/cca/capitalcityaquatics/aquariuminfo/ui/theme/Color.kt
@@ -0,0 +1,13 @@
+package cca.capitalcityaquatics.aquariuminfo.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+val BlueDK = Color(0xFF497EB0)
+val BlueLT = Color(0xFF81B2E8)
+val BlueWT = Color(0xFFD8F3FF)
+val Gold = Color(0xFFE19F20)
+val RedDK = Color(0xFF75344E)
