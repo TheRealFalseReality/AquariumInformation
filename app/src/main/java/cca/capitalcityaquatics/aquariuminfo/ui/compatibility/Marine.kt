87c2f055ee1a20a4f5887c5fac3426ecd2e5c4b3
TheRealFalseReality <NicholasWhite@capitalcityaquatics.com>
2022-07-19 18:00:47 -0400

    strings.xml

--- app/src/main/java/cca/capitalcityaquatics/aquariuminfo/ui/compatibility/Marine.kt
+++ app/src/main/java/cca/capitalcityaquatics/aquariuminfo/ui/compatibility/Marine.kt
@@ -18,6 +18,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import cca.capitalcityaquatics.aquariuminfo.R
@@ -87,7 +88,8 @@ fun FishCardsMarine(
 					modifier = Modifier
 						.padding(start = 12.dp, bottom = 12.dp)
 						.align(Alignment.Start),
-					style = MaterialTheme.typography.labelMedium
+					style = MaterialTheme.typography.labelMedium,
+					fontStyle = FontStyle.Italic
 				)
 				if (expanded) {
 					Text(
