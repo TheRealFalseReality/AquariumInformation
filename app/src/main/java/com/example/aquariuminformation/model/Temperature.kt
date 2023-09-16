225d5f8ca1b8fe1fc1d90d0b72999572be51fccd
Nick <nicholaswhite@capitalcityaquatics.com>
2023-09-15 20:53:15 -0400

    UI Tweaks - Temperature.kt

---
+++ app/src/main/java/com/example/aquariuminformation/model/Temperature.kt
@@ -0,0 +1,10 @@
+package com.example.aquariuminformation.model
+
+import com.example.aquariuminformation.navigation.Temperature
+import com.example.aquariuminformation.ui.pages.calculateCelsius
+
+//class Temperature : TemperatureClass() {
+//	fun callTemperature() {
+//		calculateCelsius()
+//	}
+//}
--- app/src/main/java/com/example/aquariuminformation/ui/pages/TemperaturePage.kt
+++ app/src/main/java/com/example/aquariuminformation/ui/pages/TemperaturePage.kt
@@ -1,16 +1,94 @@
 package com.example.aquariuminformation.ui.pages
 
+import androidx.annotation.VisibleForTesting
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.aquariuminformation.R
+import com.example.aquariuminformation.ui.commonui.PageView
 import com.example.aquariuminformation.ui.theme.AquariumInformationTheme
+import java.math.RoundingMode
+import java.text.DecimalFormat
 
 @Composable
 fun TemperaturePage() {
+	PageView {
+		TemperatureLayout()
+	}
+}
 
+@Composable
+fun TemperatureLayout(
+	modifier: Modifier = Modifier
+) {
+	Column(
+		modifier = modifier
+	) {
+		val inputTemperature by rememberSaveable {
+			mutableStateOf("0")
+		}
+		var selected by rememberSaveable {
+			mutableIntStateOf(R.string.text_celsius)
+		}
+
+		val temp = inputTemperature.toDoubleOrNull() ?: 0.0
+		val celsius = calculateCelsius(temp).toDoubleOrNull() ?: 0.0
+		val kelvinCel = calculateKelvinCel(temp).toDoubleOrNull() ?: 0.0
+		val kelvinFah = calculateKelvinFah(temp).toDoubleOrNull() ?: 0.0
+		val fahrenheit = calculateFahrenheit(temp).toDoubleOrNull() ?: 0.0
+	}
+}
+
+@VisibleForTesting
+fun calculateCelsius(
+	temp: Double,
+): String {
+	val celsius = (temp - 32) * (5.0 / 9.0)
+	val df = DecimalFormat("#.##")
+	df.roundingMode = RoundingMode.HALF_UP
+
+	return df.format(celsius)
+}
+
+@VisibleForTesting
+fun calculateFahrenheit(
+	temp: Double
+): String {
+	val fahrenheit = (temp * (9.0 / 5.0) + 32)
+	val df = DecimalFormat("#.##")
+	df.roundingMode = RoundingMode.HALF_UP
+
+	return df.format(fahrenheit)
+}
+
+@VisibleForTesting
+fun calculateKelvinFah(
+	temp: Double,
+): String {
+	val kelvin = ((temp - 32) * (5.0 / 9.0)) + 273.15
+	val df = DecimalFormat("#.##")
+	df.roundingMode = RoundingMode.HALF_UP
+
+	return df.format(kelvin)
+}
+
+@VisibleForTesting
+fun calculateKelvinCel(
+	temp: Double,
+): String {
+	val kelvin = (temp + 273.15)
+	val df = DecimalFormat("#.##")
+	df.roundingMode = RoundingMode.HALF_UP
+
+	return df.format(kelvin)
 }
 
 @Preview(showBackground = true)
